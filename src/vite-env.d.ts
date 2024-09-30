0f340bbba4a2240422cef70f641b2fd6fee0a9b0
Ayush Shrestha <ayushshrestha8920@protonmail.com>
2024-09-30 18:48:24 +0545

    env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    readonly VITE_PAT: string;
+    readonly VITE_BASE_ID: string;
+    readonly VITE_AUTH_TABLE_ID: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
